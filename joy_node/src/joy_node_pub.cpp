70226c77fdcc50971e38d5c9b06d566ca14ccae7
deeptuuk <zhouy2048@gmail.com>
2019-08-05 15:21:58 +0800

    add pi_model

--- joy_node/src/joy_node_pub.cpp
+++ joy_node/src/joy_node_pub.cpp
@@ -10,7 +10,7 @@ geometry_msgs::Twist temp_last;
 void write_callback(const sensor_msgs::Joy::ConstPtr &joy_cmd)
 {
 	temp.linear.x = joy_cmd->axes[4];
-	temp.angular.z = joy_cmd->axes[3]*3;
+	temp.angular.z = -joy_cmd->axes[3];
 }
 
 int main(int argc, char *argv[])
@@ -18,10 +18,10 @@ int main(int argc, char *argv[])
 
 	ros::init(argc, argv, "joy_node");
 	ros::NodeHandle n;
-	ros::Publisher cmd_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 50);
-	ros::Subscriber joy_sub = n.subscribe("/joy", 50, write_callback);
+	ros::Publisher cmd_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 0);
+	ros::Subscriber joy_sub = n.subscribe("/joy", 0, write_callback);
 
-	ros::Rate loop_rate(10);
+	ros::Rate loop_rate(100);
 
 	while (ros::ok())
 	{
